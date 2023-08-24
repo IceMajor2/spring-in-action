69beea122a43b1111f0946b44e733d5a03a0b51a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-24 16:00:21 +0200

    Create Spring Boot project

---
+++ taco-cloud/src/test/java/spring/in/action/taco/cloud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.in.action.taco.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
