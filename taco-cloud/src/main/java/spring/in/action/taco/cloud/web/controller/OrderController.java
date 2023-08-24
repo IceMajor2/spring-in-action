fa224608a8d670f604b99bb0310f0b7398486a9d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-24 17:16:16 +0200

    Add another controller + ingredient converter

---
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/web/controller/OrderController.java
@@ -0,0 +1,29 @@
+package spring.in.action.taco.cloud.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import spring.in.action.taco.cloud.domain.TacoOrder;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+	@GetMapping("/current")
+	public String orderForm() {
+		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder() {
+		return "redirect:/";
+	}
+}
