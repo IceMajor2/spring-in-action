f39f50f8815101976683b8be0c18703f350cd8dd
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-28 15:26:35 +0200

    Fix foreign key creation

--- taco-cloud/src/main/resources/schema.sql
+++ taco-cloud/src/main/resources/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS ingredient_ref (
 );
 
 CREATE TABLE IF NOT EXISTS ingredient (
-    id VARCHAR(4) NOT NULL,
+    id VARCHAR(4) UNIQUE NOT NULL,
     name VARCHAR(25) NOT NULL,
     type VARCHAR(10) NOT NULL
 );
