cc77f41534b2e6ad69f2de631b64dfed38cab6a9
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-28 16:57:37 +0200

    Initialize 'INGREDIENT' table using ApplicationRunner.run method

--- taco-cloud/src/main/java/spring/in/action/taco/cloud/Application.java
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/Application.java
@@ -1,13 +1,32 @@
 package spring.in.action.taco.cloud;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import spring.in.action.taco.cloud.data.IngredientRepository;
+import spring.in.action.taco.cloud.domain.Ingredient;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    public ApplicationRunner dataLoader(IngredientRepository ingredientRepository) {
+        return args -> {
+            ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+            ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+            ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+            ingredientRepository.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+            ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+            ingredientRepository.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+            ingredientRepository.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+            ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+            ingredientRepository.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+            ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+        };
+    }
 }
