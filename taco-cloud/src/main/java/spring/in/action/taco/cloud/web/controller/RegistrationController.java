20302065dceb6c9fa8003ed6de8248d2cb8bf960
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-01 16:47:30 +0200

    Create endpoint and process form

---
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/web/controller/RegistrationController.java
@@ -0,0 +1,30 @@
+package spring.in.action.taco.cloud.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.in.action.taco.cloud.data.UserRepository;
+import spring.in.action.taco.cloud.security.RegistrationForm;
+
+@Controller
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form) {
+        userRepository.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+}
