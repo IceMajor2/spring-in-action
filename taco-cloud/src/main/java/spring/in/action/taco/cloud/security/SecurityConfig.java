9429b376c542b311b0c5342710b4f749b6f0c127
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-01 17:00:24 +0200

    Configure request security

--- taco-cloud/src/main/java/spring/in/action/taco/cloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/security/SecurityConfig.java
@@ -1,16 +1,29 @@
 package spring.in.action.taco.cloud.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import spring.in.action.taco.cloud.data.UserRepository;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/design", "/orders").hasRole("USER")
+                        .requestMatchers("/", "/**").permitAll())
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/design"));
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
