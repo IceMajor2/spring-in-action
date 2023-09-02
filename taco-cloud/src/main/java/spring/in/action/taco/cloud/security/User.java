c5b337b034d443188bcb9bfa02bfb0416faaf1ad
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-02 15:53:22 +0200

    Add constraints and rename table

--- taco-cloud/src/main/java/spring/in/action/taco/cloud/security/User.java
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/security/User.java
@@ -1,6 +1,9 @@
 package spring.in.action.taco.cloud.security;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,7 +14,8 @@ import java.util.Collection;
 import java.util.List;
 
 @Data
-@Table("USER")
+@Table("_USER")
+@RequiredArgsConstructor
 public class User implements UserDetails {
 
     private static final long serialVersionUID = 1L;
@@ -19,13 +23,22 @@ public class User implements UserDetails {
     @Id
     private Long id;
 
+    @NotNull
+    @Size(min = 5, message = "Username needs to be at least 5 characters long")
     private final String username;
+    @NotNull
     private final String password;
+    @NotNull
     private final String fullname;
+    @NotNull
     private final String street;
+    @NotNull
     private final String city;
+    @NotNull
     private final String state;
+    @NotNull
     private final String zip;
+    @NotNull
     private final String phoneNumber;
 
     @Override
