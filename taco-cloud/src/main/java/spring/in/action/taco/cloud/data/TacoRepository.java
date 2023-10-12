0b893713b83653c6c6e1ea1318870f647e62b5db
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-12 13:31:34 +0200

    Implement CrudRepository

--- taco-cloud/src/main/java/spring/in/action/taco/cloud/data/TacoRepository.java
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/data/TacoRepository.java
@@ -1,9 +1,8 @@
 package spring.in.action.taco.cloud.data;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import spring.in.action.taco.cloud.domain.Taco;
 
-public interface TacoRepository
-         extends PagingAndSortingRepository<Taco, Long> {
-
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long>, CrudRepository<Taco, Long> {
 }
