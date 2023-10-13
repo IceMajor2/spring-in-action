0fd87471277cadb8e3ec5226643560f2bb1e1d9f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-13 11:38:00 +0200

    Return Optional.of on find method

--- taco-cloud-authorization/src/main/java/spring/in/action/taco/cloud/authorization/users/UserRepository.java
+++ taco-cloud-authorization/src/main/java/spring/in/action/taco/cloud/authorization/users/UserRepository.java
@@ -2,8 +2,9 @@ package spring.in.action.taco.cloud.authorization.users;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findByUsername(String username);
-
+    Optional<User> findByUsername(String username);
 }
