0b6cdd5771ff067f01dcbdfaeac2c66389c6c1f5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-13 11:38:38 +0200

    Annotate with Configuration & handle exceptions in UserDetailsService

--- taco-cloud-authorization/src/main/java/spring/in/action/taco/cloud/authorization/security/SecurityConfig.java
+++ taco-cloud-authorization/src/main/java/spring/in/action/taco/cloud/authorization/security/SecurityConfig.java
@@ -1,16 +1,19 @@
 package spring.in.action.taco.cloud.authorization.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import spring.in.action.taco.cloud.authorization.users.UserRepository;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -23,8 +26,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    UserDetailsService userDetailsService(UserRepository userRepository) {
-        return username -> userRepository.findByUsername(username);
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("User '%s' not found".formatted(username)));
     }
 
     @Bean
