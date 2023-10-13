759f3631bd4ad69723fa9d25c825c383f9623490
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-13 09:54:52 +0200

    Create Authorization Server (new project)

---
+++ taco-cloud-authorization/src/main/java/spring/in/action/taco/cloud/authorization/TacoCloudAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package spring.in.action.taco.cloud.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudAuthorizationApplication.class, args);
+	}
+
+}
---
+++ taco-cloud-authorization/src/test/java/spring/in/action/taco/cloud/authorization/TacoCloudAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.in.action.taco.cloud.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
