4971177ef0ea815b33bbdd464d8af74338c3ea0c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-21 15:17:44 +0200

    Configure resource server

--- taco-cloud/src/main/java/spring/in/action/taco/cloud/security/SecurityConfig.java
+++ taco-cloud/src/main/java/spring/in/action/taco/cloud/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -52,6 +53,7 @@ public class SecurityConfig {
                         .requestMatchers(mvc.pattern("/"), mvc.pattern("/**")).permitAll()
                         .requestMatchers(toH2Console()).permitAll()
                         .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .formLogin((formLogin) -> formLogin
                         .loginPage("/login")
                         .defaultSuccessUrl("/design"));
