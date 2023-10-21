f0c8ef6289b559fcb031689670b36448d0221afb
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-21 14:45:49 +0200

    Remove taco-admin-client OAuth 2.0 client

---
+++ taco-cloud-admin-client/src/main/java/spring/in/action/taco/cloud/admin/client/TacoCloudAdminApplication.java
@@ -0,0 +1,12 @@
+package spring.in.action.taco.cloud.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudAdminApplication.class, args);
+    }
+}
